a4b680ad0e8e100d13c66e3b682eeae09f5860d7
Nick Charlton <hello@nickcharlton.net>
2011-10-16 23:56:58 +0100

    Adds a very basic keyboard input example.

---
+++ hello_name.py
@@ -0,0 +1,3 @@
+# note: we use raw_input here, as a normal input will try to evalute what you give it as normal python, causing all manner of problems
+name = raw_input('Enter your name: ')
+print 'Hello,', name
