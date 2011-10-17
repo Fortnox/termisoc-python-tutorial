0b4d792b7775c3a46701ce938721dd8b9c75b209
Fortnox <Fortnox2005@gmail.com>
2011-10-17 14:16:21 +0200

    Woop woop, forkin' forkin'

--- adventure_game.py
+++ adventure_game.py
@@ -2,17 +2,22 @@ import random
 
 class Area:
 	inventory = []
+	description = ""
 
-	def __init__(self, name, inventory):
+	def __init__(self, name, inventory,description):
 		self.name = name
 		self.inventory = inventory
+		self.description = description
 
-	def displayInventory(self):
+	def describeArea(self):
 		i = 0
-		print "You look around, and see:"
-		for item in self.inventory:
-			print str(i) + ": " + item.name
-			i += 1
+		if(self.inventory):
+			print self.description + "\nObvious items are:"
+			for item in self.inventory:
+				print str(i) + ": " + item.name
+				i += 1
+		else:
+			print "No obvious items."
 
 class Consumable:
 	def __init__(self, name):
@@ -29,11 +34,47 @@ class Poison(Consumable):
 	def consume(self, player):
 		player.health -= 1
 
+class AreaGenerator:
+	items = [Potion("Red Potion"), Poison("Black Potion")]
+	places = [["dark woods","Dark, mysterious woods surround you."],["hill","You can see for miles atop this hill"],["ravine","Water flows quietly from a nearby ravine"],["house","You've entered an older, abandoned country cottage."],["thing","An interesting thing makes this location different and unique."]]
+	
+
+	def generateArea(self): 
+		place = random.randint(0,(len(self.places) -1))
+		noItems = random.randint(0,4)
+		items = []
+		
+		for i in range(noItems):
+			items.append(self.randomItem())
+		return Area(self.places[place][0], items,self.places[place][1])
+	
+	def randomItem(self):
+		itemNo = random.randint(0,(len(self.items) -1))
+		return self.items[itemNo]
+
+class Map:
+	map = []
+	areaGenerator = AreaGenerator()
+	
+	def __init__(self,height,length):
+		for i in range(height):
+			row = []
+			for x in range(length):
+				row.append(self.areaGenerator.generateArea())
+			self.map.append(row)
+	
+	def getArea(self,horizontal,vertical):
+		return self.map[vertical][horizontal]
+
 class Player:
 	level = 1
 	health = 10
 
-	area = Area("Test", [Potion("Test Potion"), Poison("Test Poison")])
+	map = Map(100,100)
+	horizontal = 0;
+	vertical = 0;
+	
+	area = map.getArea(vertical,horizontal)
 	inventory = []
 
 	def pickupItem(self, index):
@@ -55,26 +96,46 @@ class Player:
 	def displayStatus(self):
 		print "Level: " + str(self.level)
 		print "Health: " + str(self.health)
+	
+	def moveHorizontal(self,movement):
+		self.horizontal += movement
+		self.area = self.map.getArea(self.vertical,self.horizontal)
+		self.area.describeArea()
+	
+	def moveVertical(self,movement):
+		self.vertical += movement
+		self.area = self.map.getArea(self.vertical,self.horizontal)
+		self.area.describeArea()
+
 
 class Output:
 	def printHelp(self):
 		print "Known commands are:"
-		print "inventory"
-		print "pickup <item>"
-		print "use <item>"
-		print "quit"
-		print "help"
+		print "north,south,east,west [n,s,e,w]"
+		print "look [l]"
+		print "status [st]"
+		print "inventory [i]"
+		print "pickup <item> [p]"
+		print "use <item> [u]"
+		print "quit [q]"
+		print "help [h]"
 
 player = Player()
 output = Output()
+
+player.area.describeArea()
 
 while(True):
 	userInput = raw_input("Please enter a command:")
 	userInput = userInput.split()
 
+	if player.health < 1:
+		print "Oh dear, you've died."
+		quit()
+
 	if userInput[0] == 'inventory' or userInput[0] == 'i':
 		player.displayInventory()
-	elif userInput[0] == 'status' or userInput[0] == 's':
+	elif userInput[0] == 'status' or userInput[0] == 'st':
 		player.displayStatus()
 	elif userInput[0] == 'pickup' or userInput[0] == 'p':
 		try:
@@ -87,10 +148,18 @@ while(True):
 		except:	
 			print "That item doesn't exist!"
 	elif userInput[0] == 'look' or userInput[0] == 'l':
-		player.area.displayInventory()
+		player.area.describeArea()
 	elif userInput[0] == 'quit' or userInput[0] == 'q':
 		quit()
 	elif userInput[0] == 'help' or userInput[0] == 'h':
 		output.printHelp()
+	elif userInput[0] == 'north' or userInput[0] == 'n':
+		player.moveVertical(1)
+	elif userInput[0] == 'south' or userInput[0] == 's':
+		player.moveVertical(-1)
+	elif userInput[0] == 'east' or userInput[0] == 'e':
+		player.moveHorizontal(1)
+	elif userInput[0] == 'west' or userInput[0] == 'w':
+		player.moveHorizontal(-1)
 	else:
 		print "Uh oh! I can't find the command '" + userInput[0] + "', please enter another command."
